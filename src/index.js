220bcc8add777d88ce3e8971f867b08d98b81db7
MelissaCcoyllo <mel.ccoyllo@gmail.com>
2019-10-01 23:21:49 -0500

    segundo avance

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function Bienvenida(){  
+    let welcome= document.getElementById("Welcome");
+    let hello= document.getElementById("hello");
+    let answer=document.getElementById("answer");
+
+
+function inicio1(){
+    document.getElementById("welcome").style.display="none";
+    hello.style.display="block";
+    answer.style.display="none";
+    const name= document.getElementById("nombre").value;
+    document.getElementById("hola").innerHTML= "Hola, "+ name + " ingresa la contraseña que deseas cifrar";
+    }
+
+    if (condition) {
+        
+    } else {
+        
+    }
+    
+}
