1430b8d0cff236a5fc0d888b1417ebdb7674769e
MelissaCcoyllo <mel.ccoyllo@gmail.com>
2019-10-08 17:56:39 -0500

    sexto cambio

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,45 @@
 window.cipher = {
-encode: (string, offset) => {
-     //mensaje recibido
-    let result= "";
-     //inicio de recorrido de caracteres
-    for (let i =0; i<string.length; i++){
-         let asciiPosition= string[i];
-         if (asciiPosition.match(/[a-z]/i)) {
-         if (string.charCodeAt(i)>= 65 && string.charCodeAt(i) <= 90) {
-
-         let text= (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-         result += String.fromCharCode(text);
-     }
-         else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-          text = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
-          result += String.fromCharCode(text); 
-     }      
-     } else {
-          result += asciiPosition;
-     }
- }   
-    return result;
-     },
-
-decode: (string, offset) => {
-     //mensaje recibido
-      let result2= "";
-     //inicio de recorrido de caracteres
-     for (let i =0; i<string.length; i++){
-          let asciiPosition2= string[i];
-          if (asciiPosition2.match(/[a-z]/i)) {
-          if (string.charCodeAt(i)>= 65 && string.charCodeAt(i) <= 90) {
- 
-          let text2= (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
-          result2 += String.fromCharCode(text2);
-      }
-          else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-          let text= ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
-           result2 += String.fromCharCode(text); 
-      }      
+  encode: (string, offset) => {
+  /* mensaje recibido */
+    let result = ''
+    /* inicio de recorrido de caracteres */
+    for (let i = 0; i < string.length; i++) {
+      const asciiPosition = string[i]
+      /* Busqueda indistinta a minusculas o mayusculas /[]/expresion regular */
+      if (asciiPosition.match(/[a-z]/i)) {
+        /* condicion para cifrar en mayusculas */
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          /* obtener el numero ASCII con formula */
+          const text = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65
+          /* convertir el numero ASCII a letra */
+          result += String.fromCharCode(text)
+        }
       } else {
-           result2 += asciiPosition2;
+        result += asciiPosition
       }
-  }   
-     return result2;
+    }
+    return result
+  },
+
+  decode: (string, offset) => {
+    /* mensaje recibido */
+    let result2 = ''
+    /* inicio de recorrido de caracteres */
+    for (let i = 0; i < string.length; i++) {
+      const asciiPosition2 = string[i]
+      /* Busqueda indistinta a minusculas o mayusculas */
+      if (asciiPosition2.match(/[a-z]/i)) {
+        /* condicion para descifrar en mayusculas */
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          /* obtener el numero ASCII con formula */
+          const text2 = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65
+          /* convertir el numero ASCII a letra */
+          result2 += String.fromCharCode(text2)
+        }
+      } else {
+        result2 += asciiPosition2
+      }
+    }
+    return result2
   }
 }
